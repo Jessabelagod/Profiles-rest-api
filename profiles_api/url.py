60c6cf22607134863a36a0ac688cf0ecb8b2e4ae
Jessabel P. Agod <jessabelagod21@gmail.com>
2023-10-24 13:26:12 +0800

    Added HelloApiView

--- profiles_api/url.py
+++ profiles_api/url.py
@@ -7,7 +7,8 @@ class HelloApiView(APIView):
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
-    an_apiview = [
+
+        an_apiview = [
             'Uses HTTP methods as functions (get, post, patch, put, delete)',
             'Is similar to a traditional Django View',
             'Gives you the most control over your logic',
